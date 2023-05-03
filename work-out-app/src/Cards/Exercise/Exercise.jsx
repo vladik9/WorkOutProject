4fe8f39fed19c0925a2d9bb678e3fcc964190d2c
Vlad Cornici <v.cornici@gmail.com>
2023-05-03 19:26:00 +0300

    added styles icons and fixed position

--- work-out-app/src/Cards/Exercise/Exercise.jsx
+++ work-out-app/src/Cards/Exercise/Exercise.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import styles from "./Exercise.module.css";
 export default function Exercise({ exercise }) {
   return (
     <div>
@@ -7,8 +9,13 @@ export default function Exercise({ exercise }) {
         <span>
           <strong> {exercise.repetisions}</strong>
         </span>
-        <a href={exercise.link} target="_blank" rel="noreferrer">
-          Image
+        <a
+          href={exercise.link}
+          className={styles.iconStle}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiOutlineInfoCircle />
         </a>
       </div>
     </div>
---
+++ work-out-app/src/exercisesObject.js
@@ -0,0 +1,130 @@
+var uiqueId = require("uniqid");
+const exerciseObject = [
+  {
+    id: uiqueId(),
+    day: "Monday",
+    bodyPart: "Legs",
+    exercises: [
+      {
+        exercise: "1. Barbell squats (high trap bar)",
+        link: "https://www.inspireusafoundation.org/wp-content/uploads/2022/06/the-barbell-squat.jpg",
+        repetisions: "3x12",
+        exId: "78687462387",
+      },
+      {
+        exercise: "2. Leg press (low foot placement)",
+        link: "https://graduatefitness.com/wp-content/uploads/2021/01/IMG_2775.gif",
+        repetisions: "2x15",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "3. Deadlift with dumbbells (pause for 1 sec at the bottom)",
+        link: "https://media.tenor.com/kyOyzUcfIpMAAAAC/dumbbell-romanian.gif",
+        repetisions3: "3x8",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "4. Single-leg squats (with or without support)",
+        link: "https://i0.wp.com/thumbs.gfycat.com/MammothExaltedAyeaye-size_restricted.gif?w=1155&h=840",
+        repetisions: "3x10",
+        exId: uiqueId(),
+      },
+      {
+        exercise:
+          "5. Jumping jacks to maximum height (jumps  as high as possible)",
+        link: "https://cdn.dribbble.com/users/2931468/screenshots/5720362/jumping-jack.gif",
+        repetisions: "4x10",
+        exId: uiqueId(),
+      },
+    ],
+  },
+  {
+    id: uiqueId(),
+    day: "Wednesday",
+    bodyPart: "Chest, Biceps, Shoulders",
+    exercises: [
+      {
+        exercise: "1. Bench press with 2 sec pause at the bottom",
+        link: "https://media1.giphy.com/media/QvXVzMT3oziRDud6df/giphy.gif?cid=6c09b952aqejufxjy7st2t6eu0u226er5sl56r5tk0xxhgss&rid=giphy.gif&ct=s",
+        repetisions: "3x10",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "2.Dips (with or without additional weight) ",
+        link: "https://www.bodybuildingmealplan.com/wp-content/uploads/Weighted-Dips-scaled.jpg",
+        repetisions: "3x10",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "3.Incline dumbbell press (45°)",
+        link: "https://media.tenor.com/9T1dx6LbbgwAAAAC/2inclne-dumbel-press.gif",
+        repetisions3: "3x8",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "4. Prayer bench press",
+        link: "https://media.tenor.com/Vhhf9PFyFSwAAAAC/exercise-close-grip-bench-press.gif",
+        repetisions: "3x15",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "5. Hammer curls",
+        link: "https://www.inspireusafoundation.org/wp-content/uploads/2022/04/dumbbell-hammer-curl.gif",
+        repetisions: "3x8",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "6. Dumbbell curls with supination on incline bench (45 °)",
+        link: "https://cdn.dribbble.com/users/2931468/screenshots/5720362/jumping-jack.gif",
+        repetisions: "3x10",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "7. Standing arm swings",
+        link: "https://flabfix.com/wp-content/uploads/2019/06/Arm-Swings.gif",
+        repetisions: "2x20",
+        exId: uiqueId(),
+      },
+    ],
+  },
+  {
+    id: uiqueId(),
+    day: "Monday",
+    bodyPart: "Legs",
+    exercises: [
+      {
+        exercise: "1. Barbell squats (high trap bar)",
+        link: "https://www.inspireusafoundation.org/wp-content/uploads/2022/06/the-barbell-squat.jpg",
+        repetisions: "3x12",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "2. Leg press (low foot placement)",
+        link: "https://graduatefitness.com/wp-content/uploads/2021/01/IMG_2775.gif",
+        repetisions: "2x15",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "3. Deadlift with dumbbells (pause for 1 sec at the bottom)",
+        link: "https://media.tenor.com/kyOyzUcfIpMAAAAC/dumbbell-romanian.gif",
+        repetisions3: "3x8",
+        exId: uiqueId(),
+      },
+      {
+        exercise: "4. Single-leg squats (with or without support)",
+        link: "https://i0.wp.com/thumbs.gfycat.com/MammothExaltedAyeaye-size_restricted.gif?w=1155&h=840",
+        repetisions: "3x10",
+        exId: uiqueId(),
+      },
+      {
+        exercise:
+          "5. Jumping jacks to maximum height (jumps  as high as possible)",
+        link: "https://cdn.dribbble.com/users/2931468/screenshots/5720362/jumping-jack.gif",
+        repetisions: "4x10",
+        exId: uiqueId(),
+      },
+    ],
+  },
+];
+
+export default exerciseObject;
