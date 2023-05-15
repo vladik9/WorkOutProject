65d392b8b454892ebdac896340ed464e60345f2d
ccf6 <ccf6@email.com>
2023-05-15 16:38:56 +0300

    adding table

--- work-out-app/src/ExerciseTable/ExerciseTable.jsx
+++ work-out-app/src/ExerciseTable/ExerciseTable.jsx
@@ -12,5 +12,24 @@ export default function ExerciseTable({ dataObject }) {
       });
     })
   );
-  return <div>ExerciseTable</div>;
+  return (
+    <table class="tg">
+      <thead>
+        <tr>
+          <th class="tg-0lax">Date</th>
+          <th class="tg-0lax">Exercise</th>
+          <th class="tg-0lax">Sets</th>
+          <th class="tg-0lax">Weight</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="tg-0lax">2020</td>
+          <td class="tg-0lax">21</td>
+          <td class="tg-0lax">3x3</td>
+          <td class="tg-0lax">52ks</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
