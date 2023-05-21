0e1731f7923cf56df009bda595513ef610eefa4e
Vlad Cornici <v.cornici@gmail.com>
2023-05-21 18:45:59 +0300

    adding table and logic for removing and adding new exercises

--- work-out-app/src/Cards/Card/Card.jsx
+++ work-out-app/src/Cards/Card/Card.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 import Exercise from "../Exercise/Exercise";
 import styles from "./Card.module.css";
-import { CiRepeat } from "react-icons/ci";
-export default function Card({
-  exerciseObject,
-  handleModalExecute,
-  openTableModal,
-  handleModalExecuteShowTable,
-}) {
+export default function Card({ exerciseObject, handleModalExecute }) {
   return (
     <div className={styles.contentWraper}>
       <div className={styles.tileStyle}>
         <h3>
           {exerciseObject.day}
-          <div
-            style={{ textAlign: "right" }}
-            onClick={() => handleModalExecuteShowTable(exerciseObject.day)}
-          >
-            <CiRepeat />
-          </div>
+          <div style={{ textAlign: "right" }}></div>
         </h3>
         <h4> {exerciseObject.bodyPart}</h4>
       </div>
--- work-out-app/src/Cards/Cards.jsx
+++ work-out-app/src/Cards/Cards.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import Card from "./Card/Card";
-export default function Cards({
-  dataObject,
-  handleModalExecute,
-  handleModalExecuteShowTable,
-}) {
+export default function Cards({ dataObject, handleModalExecute }) {
   return dataObject.map((el) => {
     return (
       <div key={el.id}>
-        <Card
-          exerciseObject={el}
-          handleModalExecute={handleModalExecute}
-          handleModalExecuteShowTable={handleModalExecuteShowTable}
-        />
+        <Card exerciseObject={el} handleModalExecute={handleModalExecute} />
       </div>
     );
   });
